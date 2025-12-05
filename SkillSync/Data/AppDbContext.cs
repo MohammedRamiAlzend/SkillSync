7bc48bcd9296ee8f3b484248c0de1c5dd3da8b65
Manya Farah <manyafarah28@gmail.com>
2025-12-05 17:31:29 +0100

    remove userole

--- SkillSync/Data/AppDbContext.cs
+++ SkillSync/Data/AppDbContext.cs
@@ -14,7 +14,6 @@ namespace SkillSync.Data
 
         public DbSet<User> Users => Set<User>();
         public DbSet<Role> Roles => Set<Role>();
-        public DbSet<UserRole> UserRoles => Set<UserRole>();
         public DbSet<Design> Designs => Set<Design>();
         public DbSet<Attachment> Attachments => Set<Attachment>();
 
