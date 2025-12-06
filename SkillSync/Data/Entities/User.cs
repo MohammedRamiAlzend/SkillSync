b5581fc2e13f2394c4b519366bbcd3d63ff73536
Manya Farah <manyafarah28@gmail.com>
2025-12-06 11:55:11 +0100

    remove class UserRole and implement many to many realation using ef core

--- SkillSync/Data/Entities/User.cs
+++ SkillSync/Data/Entities/User.cs
@@ -11,7 +11,7 @@
         public bool EmailConfirmed { get; set; } = false;
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+        public ICollection<Role> UserRoles { get; set; } = new List<Role>();
         public ICollection<Design> Designs { get; set; } = new List<Design>();
 
     }
